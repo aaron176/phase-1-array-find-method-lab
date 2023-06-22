dcfdc465383cbe0931919bc1e94b24b05148134f
haroon <haroon.seena2017@gmail.com>
2023-06-22 16:58:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+function superbowlWin(year, result){
+for(const user of year){
+    if(user.result === result){
+     console.log(user.year);
+    }
+}
+}
+superbowlWin(record, "W");
